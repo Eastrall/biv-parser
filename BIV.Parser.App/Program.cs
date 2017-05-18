e1693cdb953e9517a426ff050999474befa71a32
Filipe Gomes Peixoto <gomespeixoto.filipe@gmail.com>
2017-05-18 21:55:29 +0200

    Add file example. Ignore comments

--- BIV.Parser.App/Program.cs
+++ BIV.Parser.App/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BIV.Parser.Core;
+using System;
 
 namespace BIV.Parser.App
 {
@@ -6,7 +7,52 @@ namespace BIV.Parser.App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var file = new BivFile("../example.biv"))
+            {
+                file.Parse();
+
+                //foreach (var statement in file.Statements)
+                //    Display(statement);
+            }
+
+            Console.ReadLine();
+        }
+
+        static void Display(IStatement statement, int index = 0)
+        {
+            DisplayTreeBranches(index);
+
+            switch (statement.Type)
+            {
+                case StatementType.Block:
+                    var block = statement as Block;
+
+                    foreach (var blockStatement in block.Statements)
+                        Display(blockStatement, index + 1);
+                    break;
+                case StatementType.Instruction:
+                    Console.WriteLine("[Instruction]: {0} => ", statement.Name);
+                    // Todo: display params
+                    break;
+                case StatementType.Variable:
+                    var variable = statement as Variable;
+
+                    Console.WriteLine("[Variable]: {0} = {1}", variable.Name, variable.Value);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        static void DisplayTreeBranches(int branch)
+        {
+            if (branch > 0)
+            {
+                Console.Write(Environment.NewLine + "|");
+                for (int i = 0; i < branch; i++)
+                    Console.Write("-");
+                Console.Write(" ");
+            }
         }
     }
 }
---
+++ BIV.Parser.Core/BivFile.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BIV.Parser.Core
+{
+    public sealed class BivFile : IDisposable
+    {
+        private const string SingleLineComment = "//";
+        private const string MultiLineCommentBegin = "/*";
+        private const string MultiLineCommentEnd = "*/";
+
+        private string filePath;
+
+        public ICollection<IStatement> Statements { get; private set; }
+
+        public BivFile(string filePath)
+        {
+            this.filePath = filePath;
+
+            this.Statements = new List<IStatement>();
+        }
+
+        public void Parse()
+        {
+            using (var fileStream = new FileStream(this.filePath, FileMode.Open, FileAccess.Read))
+            using (var reader = new StreamReader(fileStream))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine().Trim();
+
+                    if (line.StartsWith(SingleLineComment))
+                        continue;
+                    if (line.Contains(MultiLineCommentBegin))
+                    {
+                        line = null;
+                        while (!reader.ReadLine().Trim().Contains(MultiLineCommentEnd))
+                            continue;
+                    }
+
+                    Console.WriteLine(line);
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
