323564c01d9a739b1a71646d9f4be67a2c7d3136
Filipe Gomes Peixoto <gomespeixoto.filipe@gmail.com>
2017-05-18 22:25:14 +0200

    Update example file

--- BIV.Parser.Core/BivFile.cs
+++ BIV.Parser.Core/BivFile.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace BIV.Parser.Core
@@ -10,8 +11,10 @@ namespace BIV.Parser.Core
         private const string SingleLineComment = "//";
         private const string MultiLineCommentBegin = "/*";
         private const string MultiLineCommentEnd = "*/";
+        private readonly char[] SplitCharacters = new char[] { '\n', '\r' };
 
         private string filePath;
+        private string[] fileContent;
 
         public ICollection<IStatement> Statements { get; private set; }
 
@@ -24,25 +27,29 @@ namespace BIV.Parser.Core
 
         public void Parse()
         {
+            this.ReadFile();
+
+            if (this.fileContent.Length <= 0)
+                return;
+
+            for (int i = 0; i < this.fileContent.Length; i++)
+            {
+                string line = this.fileContent[i];
+
+                if (line.StartsWith(SingleLineComment))
+                    continue;
+
+                if (line.Contains(SingleLineComment))
+                    line = line.Remove(line.IndexOf(SingleLineComment));
+            }
+        }
+
+        private void ReadFile()
+        {
             using (var fileStream = new FileStream(this.filePath, FileMode.Open, FileAccess.Read))
             using (var reader = new StreamReader(fileStream))
-            {
-                while (!reader.EndOfStream)
-                {
-                    string line = reader.ReadLine().Trim();
-
-                    if (line.StartsWith(SingleLineComment))
-                        continue;
-                    if (line.Contains(MultiLineCommentBegin))
-                    {
-                        line = null;
-                        while (!reader.ReadLine().Trim().Contains(MultiLineCommentEnd))
-                            continue;
-                    }
-
-                    Console.WriteLine(line);
-                }
-            }
+                this.fileContent = (from x in reader.ReadToEnd().Split(SplitCharacters, StringSplitOptions.RemoveEmptyEntries)
+                                    select x.Trim()).ToArray();
         }
 
         public void Dispose()
